3615d3b0431d96d9a4a5b3b85f66fc628bbbf0dd
dmhacker <dmhacker@yahoo.com>
2018-01-29 18:41:33 -0800

    Start using slack_hook

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate slack_hook;
+use slack_hook::{Slack, PayloadBuilder};
+
 fn main() {
-    println!("Hello, world!");
+    let slack = Slack::new("https://hooks.slack.com/services/abc/123/45z").unwrap();
+    let p = PayloadBuilder::new()
+      .text("test message")
+      .channel("#testing")
+      .username("My Bot")
+      .icon_emoji(":chart_with_upwards_trend:")
+      .build()
+      .unwrap();
+
+    let res = slack.send(&p);
+    match res {
+        Ok(()) => println!("ok"),
+        Err(x) => println!("ERR: {:?}",x)
+    }
 }
