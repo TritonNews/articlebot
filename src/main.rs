b583c744a9965ea7658aab1108862f10db0c809f
David Hacker <dmhacker.cs@gmail.com>
2018-01-31 12:21:49 -0800

    Start using reqwest framework for the Trello bot

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
 extern crate slack;
+extern crate reqwest;
+
+mod trello;
+
 use slack::{Event, EventHandler, RtmClient};
+use trello::Board;
+use std::env;
 
 struct SlackArticleHandler;
 
@@ -18,18 +27,19 @@ impl EventHandler for SlackArticleHandler {
 }
 
 fn main() {
-    let api_key = std::env::var("SLACK_API_KEY");
-    match api_key {
-      Ok(key) => {
-        let mut handler = SlackArticleHandler;
-        let client = RtmClient::login_and_run(&key, &mut handler);
-        match client {
-            Ok(_) => {}
-            Err(err) => panic!("Error: {}", err),
-        }
-      }
-      Err(err) => {
-        panic!("Error: {}", err)
-      }
+    let slack_api_key = env::var("SLACK_API_KEY").expect("Slack API key not found");
+    let trello_api_key = env::var("TRELLO_API_KEY").expect("Trello API key not found");
+    let trello_oauth_token = env::var("TRELLO_OAUTH_TOKEN").expect("Trello OAuth token not found");
+    let trello_board_id = env::var("TRELLO_BOARD_ID").expect("Trello board ID not found");
+
+    let mut board = Board::new(&trello_board_id, &trello_api_key, &trello_oauth_token);
+    board.load();
+    board.start_tracking();
+
+    let mut handler = SlackArticleHandler;
+    let client = RtmClient::login_and_run(&slack_api_key, &mut handler);
+    match client {
+        Ok(_) => {}
+        Err(err) => panic!("Error: {}", err),
     }
 }
--- src/trello.rs
+++ src/trello.rs
@@ -1,1 +1,55 @@
-extern crate reqwest;
+use reqwest::Client;
+use reqwest::header::UserAgent;
+use reqwest::Result;
+
+const USER_AGENT: &'static str =
+  "Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586";
+
+#[derive(Deserialize)]
+struct BoardProperties {
+  id: String,
+  name: String
+}
+
+pub struct Board {
+  properties: BoardProperties,
+  http_url: String,
+  http_token_parameters: String,
+  http_client: Client
+}
+
+impl Board {
+
+  pub fn new(board_id: &str, trello_api_key: &str, trello_oauth_token: &str) -> Board {
+    let properties = BoardProperties {
+      id: board_id.to_string(),
+      name: "".to_string()
+    };
+
+    Board {
+      properties: properties,
+      http_url: format!("https://api.trello.com/1/boards/{}", board_id).to_string(),
+      http_token_parameters: format!("key={}&token={}", trello_api_key, trello_oauth_token).to_string(),
+      http_client: Client::new()
+    }
+  }
+
+  pub fn load(&mut self) -> Result<()> {
+    let mut resp = self.http_client
+      .get(&format!("{}?fields=name&{}", self.http_url, self.http_token_parameters))
+      .header(UserAgent::new(USER_AGENT.to_string()))
+      .send()?;
+
+    self.properties = resp.json()?;
+
+    Ok(())
+  }
+
+  pub fn start_tracking(&mut self) -> Result<()> {
+    Ok(())
+  }
+}
+
+pub trait BoardActionHandler {
+
+}
