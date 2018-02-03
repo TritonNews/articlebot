9c266aee25c95483ac516ed329feedc6f59b6283
David Hacker <dmhacker.cs@gmail.com>
2018-02-03 13:39:17 -0800

    Add /track and /hello commands

--- src/main.rs
+++ src/main.rs
@@ -28,81 +28,89 @@ struct SlackHandler {
 
 impl EventHandler for SlackHandler {
     fn on_event(&mut self, cli: &RtmClient, event: Event) {
-        match event {
-            Event::Message(boxed_message) => {
-                match *boxed_message {
-                    Message::Standard(message) => {
-                        let tracker = message.user.unwrap();
-                        let tracking = message.text.unwrap();
-                        let channel = message.channel.unwrap();
+        let sender = cli.sender();
+        if let Event::Message(boxed_message) = event {
+            if let Message::Standard(message) = *boxed_message {
+                let text = message.text.unwrap();
+                let channel = message.channel.unwrap();
 
-                        // Slack collection in MongoDB (key: tracker id, other data: channel id, tracking name)
-                        let slack_coll = self.db.collection("slack");
-                        let slack_lookup = doc! {
-                            "uid": &tracker
+                let args: Vec<&str> = text.split(" ").collect();
+                let command = args[0];
+
+                if command == "/hello" {
+                    sender.send_message(&channel[..], "Hello there.").expect("Slack sender error");
+                }
+                else if command == "/track" {
+                    let tracker = message.user.unwrap();
+                    let tracking = (&args[1..]).join(" ");
+
+                    // Slack collection in MongoDB (key: tracker id, other data: channel id, tracking name)
+                    let slack_coll = self.db.collection("slack");
+                    let slack_lookup = doc! {
+                        "uid": &tracker
+                    };
+
+                    // Trello collection in MongoDB (key: tracking name, other data: list of trackers)
+                    let trello_coll = self.db.collection("trello");
+                    let trello_lookup = doc! {
+                        "name": &tracking
+                    };
+
+                    // Delete all previous records in the Slack and Trello collections if they exist
+                    if let Some(sdoc) = slack_coll.find_one_and_delete(slack_lookup, None).expect("Failed to delete document") {
+                        let trello_lookup_old = doc! {
+                            "tracking": sdoc.get_str("tracking").unwrap()
                         };
+                        if let Some(tdoc) = trello_coll.find_one(Some(trello_lookup_old.clone()), None).expect("Failed to find document") {
+                            // Remove our current tracker from where it was originally tracking
+                            let mut trackers_old = tdoc.get_array("trackers").unwrap().clone();
+                            let index = trackers_old.iter().position(|tracker_old| *tracker_old.as_str().unwrap() == tracker).unwrap();
+                            trackers_old.remove(index);
 
-                        // Trello collection in MongoDB (key: tracking name, other data: list of trackers)
-                        let trello_coll = self.db.collection("trello");
-                        let trello_lookup = doc! {
-                            "name": &tracking
-                        };
-
-                        // Delete all previous records in the Slack and Trello collections if they exist
-                        if let Some(sdoc) = slack_coll.find_one_and_delete(slack_lookup, None).expect("Failed to delete document") {
-                            let trello_lookup_old = doc! {
-                                "tracking": sdoc.get_str("tracking").unwrap()
-                            };
-                            if let Some(tdoc) = trello_coll.find_one(Some(trello_lookup_old.clone()), None).expect("Failed to find document") {
-                                // Remove our current tracker from where it was originally tracking
-                                let mut trackers_old = tdoc.get_array("trackers").unwrap().clone();
-                                let index = trackers_old.iter().position(|tracker_old| *tracker_old.as_str().unwrap() == tracker).unwrap();
-                                trackers_old.remove(index);
-
-                                // If the Trello user has no trackers, delete it. Otherwise, update it to reflect the changes made to its changes.
-                                if trackers_old.is_empty() {
-                                    trello_coll.delete_one(trello_lookup_old, None).expect("Failed to delete document");
-                                }
-                                else {
-                                    let mut tdoc_new = tdoc.clone();
-                                    tdoc_new.insert_bson("trackers".to_string(), Bson::Array(trackers_old));
-                                    trello_coll.update_one(trello_lookup_old, tdoc_new, None).expect("Failed to update document");
-                                }
+                            // If the Trello user has no trackers, delete it. Otherwise, update it to reflect the changes made to its changes.
+                            if trackers_old.is_empty() {
+                                trello_coll.delete_one(trello_lookup_old, None).expect("Failed to delete document");
+                            }
+                            else {
+                                let mut tdoc_new = tdoc.clone();
+                                tdoc_new.insert_bson("trackers".to_string(), Bson::Array(trackers_old));
+                                trello_coll.update_one(trello_lookup_old, tdoc_new, None).expect("Failed to update document");
                             }
                         }
+                    }
 
-                        // Insert a new Slack document specifying the tracker's information
-                        slack_coll.insert_one(doc! {
-                            "uid": &tracker,
-                            "cid": &channel,
-                            "tracking": &tracking
+                    // Insert a new Slack document specifying the tracker's information
+                    slack_coll.insert_one(doc! {
+                        "uid": &tracker,
+                        "cid": &channel,
+                        "tracking": &tracking
+                    }, None).expect("Failed to insert document");
+
+                    // Update (or create/insert) the Trello document that contains the trackers
+                    if let Some(tdoc) = trello_coll.find_one(Some(trello_lookup.clone()), None).expect("Failed to find document") {
+                        let mut trackers = tdoc.get_array("trackers").unwrap().clone();
+                        trackers.push(Bson::String(tracker));
+
+                        let mut tdoc_new = tdoc.clone();
+                        tdoc_new.insert_bson("trackers".to_string(), Bson::Array(trackers));
+
+                        trello_coll.update_one(trello_lookup, tdoc_new, None).expect("Failed to update document");
+                    }
+                    else {
+                        trello_coll.insert_one(doc! {
+                            "name": &tracking,
+                            "trackers": [&tracker]
                         }, None).expect("Failed to insert document");
+                    }
 
-                        // Update (or create/insert) the Trello document that contains the trackers
-                        if let Some(tdoc) = trello_coll.find_one(Some(trello_lookup.clone()), None).expect("Failed to find document") {
-                            let mut trackers = tdoc.get_array("trackers").unwrap().clone();
-                            trackers.push(Bson::String(tracker));
-
-                            let mut tdoc_new = tdoc.clone();
-                            tdoc_new.insert_bson("trackers".to_string(), Bson::Array(trackers));
-
-                            trello_coll.update_one(trello_lookup, tdoc_new, None).expect("Failed to update document");
-                        }
-                        else {
-                            trello_coll.insert_one(doc! {
-                                "name": &tracking,
-                                "trackers": [&tracker]
-                            }, None).expect("Failed to insert document");
-                        }
-
-                        let sender = cli.sender();
-                        sender.send_message(&channel[..], &format!("You will now be notified when {}'s articles are moved in Trello.", tracking)[..])
-                            .expect("Slack sender error");
-                    },
-                    _ => ()
+                    sender.send_message(&channel[..], &format!("You will now be notified when {}'s articles are moved in Trello.", tracking)[..])
+                        .expect("Slack sender error");
                 }
-            },
-            _ => ()
+                else {
+                    sender.send_message(&channel[..], &format!("I did not understand your command \"{}\".", command)[..])
+                        .expect("Slack sender error");
+                }
+            }
         }
     }
 
